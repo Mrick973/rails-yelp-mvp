115a3aa99a3bae5b4a6f7a18fde0c7f6888d23ae
Hemrick Donatien <do.hemrick@gmail.com>
2018-07-25 15:51:43 -0300

    RailsModeleFinish

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
+  validates :content, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5],
-    message: "is not a valid" }
+    message: "is not a valid" }, numericality: {only_integer: true }
 end
